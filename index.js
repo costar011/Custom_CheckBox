76f28f6836f236bde55ecd11e5cbd09303387f3f
costar011 <dnaer456@gmail.com>
2023-09-08 10:31:12 +0900

    js

---
+++ index.js
@@ -0,0 +1,11 @@
+const emojiInput = document.getElementById("emojiInput");
+const emoji = document.getElementById("emoji");
+emojiInput.addEventListener('click', () => {
+    console.log(emojiInput.checked);
+
+    if(emojiInput.checked == true) {
+        emoji.innerHTML = '😊';
+    } else {
+        emoji.innerHTML = '😣';
+    }
+});
